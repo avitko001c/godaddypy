e6cbab8cab84955c7681946d880e73dbe5c37c5a
avitko001 <andrewvitko@gmail.com>
2021-01-18 04:01:55 -0500

    Added a configloader to store api and secret keys

--- godaddypy/account.py
+++ godaddypy/account.py
@@ -1,3 +1,7 @@
+import os
+import pathlib
+from godaddypy.configloader import load_config
+
 __all__ = ['Account']
 
 
@@ -6,12 +10,20 @@ class Account(object):
 
     An account is used to provide authentication headers to the `godaddypy.Client`.
     """
-    _api_key = None
-    _api_secret = None
+    configfile = pathlib.Path(os.path.expanduser('~/.godaddy/credentials'))
+    if configfile.exists():
+        try:
+            config = load_config(configfile)
+            _api_key = config['profiles']['default']['api_key']
+            _api_secret = config['profiles']['default']['api_secret']
+        except:
+            _api_key = None
+            _api_secret = None
+            pass
 
     _SSO_KEY_TEMPLATE = 'sso-key {api_key}:{api_secret}'
 
-    def __init__(self, api_key, api_secret, delegate=None):
+    def __init__(self, api_key=_api_key, api_secret=_api_secret, delegate=None):
         """Create a new `godadypy.Account` object.
 
         :type api_key: str or unicode
